04ede03b248be5b014084a66e922aac536f58711
Rohitash Goyal <32011743+Rohitash28@users.noreply.github.com>
2021-03-09 10:21:08 +0530

    fifth

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -19,7 +19,7 @@
       };
       getDateData = function(endDate) {
         var dateData, diff;
-        endDate = Date.parse($.isPlainObject(_this.options.date) ? _this.options.date : new Date(2021,4,11));
+        endDate = Date.parse($.isPlainObject(_this.options.date) ? _this.options.date : new Date(2021,3,11));
         diff = (endDate - Date.parse(new Date)) / 1000;
         if (diff <= 0) {
           diff = 0;
